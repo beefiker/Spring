7abe8bdd73e8b82e2ec6a7206600d69117931d58
J young <beefiker@gmail.com>
2021-04-08 07:17:50 +0900

    Added findDuplicated bean test

--- src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -25,6 +25,8 @@ public class ApplicationContextInfoTest {
         String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
             BeanDefinition beanDefinition = applicationContext.getBeanDefinition(beanDefinitionName);
+            // BeanDefinition.ROLE_APPLICATION : 직접 등록한 애플리케이션 빈
+            // BeanDefinition.ROLE_INFRASTRUCTURE : 스프링이 내부에서 사용하는 빈
             if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
                 Object bean = applicationContext.getBean(beanDefinitionName);
                 System.out.println("Name = " + beanDefinitionName + " object = " + bean);
